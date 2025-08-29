ed05bc6eb560459221bc90231b1a18ef349eb94f
slawomirmiszta39-web <slawomirmiszta39@gmail.com>
2025-08-29 13:55:54 +0200

    Create main.tsx

---
+++ src/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+function App() {
+  return (
+    <h1>Hello MISZTA!</h1>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
